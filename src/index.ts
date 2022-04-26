dc0d79ac1b0970477fd5ab4b9b199b972d2208b3
sunny-pandora <sunny.jang@pandroa.tv>
2022-04-26 15:21:40 +0900

    인터페이스 & 클래스 설정

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,32 @@
-const name = "Sunny",
-    age = 24,
-    gender = "female";
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
 
-const sayHi = (name: string, age: number, gender?: string): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-console.log(sayHi("Sunny", age, "femaie"));
+const sunny = new Human("Sunny", 28, "female");
+interface Human {
+    name: string,
+    age: number,
+    gender: string
+}
+
+const person = {
+    name: "Sunny",
+    age: 24,
+    gender: "female"
+};
+
+const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+}
+
+console.log(sayHi(sunny));
 
 export { };
