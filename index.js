09af950f8f07883772abd4c3e7efb4136205fff2
sunny-pandora <sunny.jang@pandroa.tv>
2022-04-26 14:59:35 +0900

    index type 설정

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Sunny", age = 24, gender = "female";
+const sayHi = (name, age, gender) => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+};
+sayHi("Sunny", age, "femaie");
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+const name = "Sunny",
+    age = 24,
+    gender = "female";
+
+const sayHi = (name: string, age: number, gender?: string): string => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+}
+
+sayHi("Sunny", age, "femaie");
+
+export { };
