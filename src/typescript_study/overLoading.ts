b2a231513c9afbcc836e158e5f945b6a12d1d468
Sunny Jang <esunbest@naver.com>
2022-04-28 17:44:53 +0900

    오버라이딩 기법

---
+++ src/typescript_study/overLoading.ts
@@ -0,0 +1,16 @@
+type Config = {
+    path: string,
+    state: object
+}
+
+type Push = {
+    (path: string): void
+    (config: Config): void
+}
+
+const push: Push = (config) => {
+    if (typeof config === "string") { console.log(config) }
+    else {
+        console.log(config.path);
+    }
+}
